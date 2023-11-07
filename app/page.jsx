7aa228d7c1b34e250df2ca281858d2f9de8bb4db
Ben Johnson <monk.sound.design@gmail.com>
2023-11-06 21:56:50 -0500

    nav design and homepage headings

--- app/page.jsx
+++ app/page.jsx
@@ -5,13 +5,28 @@ export default function Home() {
   return (
     <>
       <header>
-        <nav className="flex gap-10">
-          <a href="/">Home</a>
-          <a href="/store">Store</a>
-          <a href="/login">Login</a>
+        <nav className="flex gap-10 py-2 px-4">
+          <a href="/" className="hover:text-purple-700 transition-all">
+            Home
+          </a>
+          <a href="/store" className="hover:text-purple-700 transition-all">
+            Store
+          </a>
+          <a href="/login" className="hover:text-purple-700 transition-all">
+            Login
+          </a>
+          {/* ADD CART ICON */}
         </nav>
+        <div className="w-full h-[2px] bg-black"></div>
       </header>
-      <main></main>
+      <main>
+        <div className="pt-6 px-10">
+          <h1 className="text-[2.4rem] font-bold">The Chop House</h1>
+          <h2 className="text-[1.2rem] font-bold pl-2">
+            Premium cuts delivered to your door.
+          </h2>
+        </div>
+      </main>
     </>
   );
 }

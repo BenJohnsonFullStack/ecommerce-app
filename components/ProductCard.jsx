c2f6032f517b18ff139a07e0dcea7d5b992d2d11
Ben Johnson <monk.sound.design@gmail.com>
2023-11-08 21:06:06 -0500

    basic card content styles

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -9,27 +9,35 @@ const ProductCard = ({ title, sku, price, description, weight, image }) => {
       <div id="image" className="w-20">
         <Image width={1620} height={1080} alt="product image" src={image} />
       </div>
-      <div id="details">
-        <div id="flex">
-          <div id="headers">
-            <h2 id="title">{title}</h2>
-            <p id="SKU">SKU: {sku}</p>
-          </div>
+      <div className="px-2">
+        <div id="details">
+          <div id="flex" className="flex justify-between items-center">
+            <div id="headers">
+              <h2 id="title" className="text-lg font-bold">
+                {title}
+              </h2>
+            </div>
 
-          <div>
-            <h3 id="price">{price}</h3>
+            <div>
+              <h3 id="price" className="font-bold">
+                {price}
+              </h3>
+            </div>
           </div>
-        </div>
-
-        <div id="description">
-          <p>{description}</p>
-          <p>
-            <span>Net Weight: {weight}</span>
+          <p id="SKU" className="text-sm">
+            SKU: {sku}
           </p>
-        </div>
 
-        <div id="order">
-          <button id="addToCart"></button>
+          <div id="description">
+            <p>{description}</p>
+            <p>
+              <span className="italic text-sm">Net Weight: {weight}</span>
+            </p>
+          </div>
+
+          <div id="order">
+            <button id="addToCart"></button>
+          </div>
         </div>
       </div>
     </div>

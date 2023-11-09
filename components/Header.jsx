518683ca99c710201ee454014ac9ab5778933da2
Ben Johnson <monk.sound.design@gmail.com>
2023-11-09 16:56:56 -0500

    active states on navigation for links

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,27 +1,38 @@
+"use client";
+
 import Image from "next/image";
 import Divider from "./Divider";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
+  const pathname = usePathname();
+
   return (
     <header>
       <nav className="py-4 px-10 flex justify-between items-center">
         <div className="flex gap-10">
           <Link
             href="/"
-            className="hover:text-red-600 transition-all font-semibold"
+            className={`hover:text-red-600 transition-all font-semibold ${
+              pathname === "/" ? "text-red-600" : ""
+            }`}
           >
             Home
           </Link>
           <Link
             href="/store"
-            className="hover:text-red-600 transition-all font-semibold"
+            className={`hover:text-red-600 transition-all font-semibold ${
+              pathname === "/store" ? "text-red-600" : ""
+            }`}
           >
             Store
           </Link>
           <Link
             href="/login"
-            className="hover:text-red-600 transition-all font-semibold"
+            className={`hover:text-red-600 transition-all font-semibold ${
+              pathname === "/login" ? "text-red-600" : ""
+            }`}
           >
             Login
           </Link>

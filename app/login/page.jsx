e5be6c94360f827f73089ecabdd59eec2f79fc91
Ben Johnson <monk.sound.design@gmail.com>
2023-11-09 09:54:46 -0500

    basic login form layout

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -5,7 +5,56 @@ const Login = () => {
   return (
     <>
       <Header />
-      <main></main>
+      <main>
+        <div
+          id="login-card"
+          className="w-[min(100%-2em,20rem)] shadow-lg bg-gray-200 px-2 text-center my-20 mx-auto"
+        >
+          <div>
+            <h2>Welcome Back!</h2>
+            <h3>Please login to your account.</h3>
+          </div>
+
+          <div>
+            <form>
+              <div>
+                <div>
+                  <label htmlFor="email">Email Address</label>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    className="border-2 border-black w-2/3 mx-auto"
+                  />
+                </div>
+
+                <div className="flex flex-col">
+                  <label htmlFor="password">Account Password</label>
+                  <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    className="border-2 border-black w-2/3 mx-auto"
+                  />
+                </div>
+
+                <div>
+                  <button>Login</button>
+                </div>
+
+                <div>
+                  <p>-- OR --</p>
+                </div>
+
+                <div>
+                  {/* GOOGLE LOGIN BUTTON */}
+                  {/* GITHUB LOGIN BUTTON */}
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </main>
       <Footer />
     </>
   );

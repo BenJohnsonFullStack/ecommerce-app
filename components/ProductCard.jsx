cc22b54f698536b47506381994350d5e2c8fe09d
Ben Johnson <monk.sound.design@gmail.com>
2023-11-08 19:06:36 -0500

    card test layout

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -2,9 +2,12 @@ import Image from "next/image";
 
 const ProductCard = ({ title, sku, price, description, weight, image }) => {
   return (
-    <div id="card">
+    <div
+      id="card"
+      className="border-2 border-black rounded-md w-[min(100%-1rem,20rem)] mx-auto"
+    >
       <div id="image" className="w-20">
-        <Image width={1920} height={1080} alt="product image" src={image} />
+        <Image width={1620} height={1080} alt="product image" src={image} />
       </div>
       <div id="details">
         <div id="flex">
@@ -21,7 +24,7 @@ const ProductCard = ({ title, sku, price, description, weight, image }) => {
         <div id="description">
           <p>{description}</p>
           <p>
-            <span>{weight}</span>
+            <span>Net Weight: {weight}</span>
           </p>
         </div>
 

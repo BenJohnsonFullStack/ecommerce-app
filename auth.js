43efe615a526740566d14efed5c9bad06fc194f3
Ben Johnson <monk.sound.design@gmail.com>
2023-11-15 14:10:21 -0500

    getUser db query

--- auth.js
+++ auth.js
@@ -3,5 +3,17 @@ import { authConfig } from "./auth.config";
 import Credentials from "next-auth/providers/credentials";
 import { z } from "zod";
 import bcrypt from "bcrypt";
+import { PrismaClient } from "@prisma/client";
 
-const getUser = async (email) => {};
+const prisma = new PrismaClient();
+
+// get user from db
+const getUser = async (email) => {
+  try {
+    const user = await prisma.findOne(email);
+    return user;
+  } catch (err) {
+    console.error("Failed to get user", err);
+    throw new Error("Failed to get user.");
+  }
+};

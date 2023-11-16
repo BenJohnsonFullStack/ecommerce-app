953644a2b9134c69234ebc8432ecfb1267ea1265
Ben Johnson <monk.sound.design@gmail.com>
2023-11-16 12:58:36 -0500

    empty cart UI

--- components/ShoppingCart.jsx
+++ components/ShoppingCart.jsx
@@ -29,26 +29,32 @@ const ShoppingCart = () => {
         )}
         {isOpen && (
           <div className="bg-black rounded-md absolute top-6 right-4 z-10 p-6">
-            {products.map((product) => (
-              <div key={product.sku} className="my-6">
-                <div className="border-t-2 border-t-white my-1"></div>
-                <div className="flex gap-10 items-center px-4">
-                  <div className="text-white">
-                    <h3 className="text-xs">SKU: {product.sku}</h3>
-                    <h3 className="mt-4 text-xs italic">
-                      QTY: {product.quantity}
-                    </h3>
+            {cartQuantity === 0 ? (
+              <h3 className="text-white text-center">Cart Empty</h3>
+            ) : (
+              <>
+                {products.map((product) => (
+                  <div key={product.sku} className="my-6">
+                    <div className="border-t-2 border-t-white my-1"></div>
+                    <div className="flex gap-10 items-center px-4">
+                      <div className="text-white">
+                        <h3 className="text-xs">SKU: {product.sku}</h3>
+                        <h3 className="mt-4 text-xs italic">
+                          QTY: {product.quantity}
+                        </h3>
+                      </div>
+                      <button
+                        className="bg-red-500 text-sm px-4 py-1 rounded-md"
+                        onClick={() => removeAll(product.sku)}
+                      >
+                        Delete
+                      </button>
+                    </div>
+                    <div className="border-t-2 border-t-white mt-2"></div>
                   </div>
-                  <button
-                    className="bg-red-500 text-sm px-4 py-1 rounded-md"
-                    onClick={() => removeAll(product.sku)}
-                  >
-                    Delete
-                  </button>
-                </div>
-                <div className="border-t-2 border-t-white mt-2"></div>
-              </div>
-            ))}
+                ))}
+              </>
+            )}
           </div>
         )}
       </div>

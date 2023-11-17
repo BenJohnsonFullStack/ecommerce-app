1bae0ca9b277ce88a171217a40e4ed5e0cec48e3
Ben Johnson <monk.sound.design@gmail.com>
2023-11-17 13:11:08 -0500

    success and cancel routes for stripe payments

---
+++ app/cancel/page.jsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const page = () => {
+  return (
+    <>
+      <h1 className="text-red-700">Payment Cancelled.</h1>
+      <div className="w-1/2 mx-auto text-center bg-red-300 shadow-lg rounded-md mt-8 text-black py-2 cursor-pointer hover:bg-red-600 transition-color duration-200">
+        <Link href="/">Back to HomePage</Link>
+      </div>
+    </>
+  );
+};
+
+export default page;
